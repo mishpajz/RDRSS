06adc360e6f997808b088132a07360fbc62a94f9
Michal Dobes <captainmishan@gmail.com>
2019-12-14 19:55:52 +0100

    Update setup.py for 0.2 release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='RDRSS',
-    version='0.1',
+    version='0.2',
     author='Michal Dobes',
     scripts=['rdrss/RDRSS.py'],
     install_requires=[
@@ -11,5 +11,6 @@ setup(
           'argparse',
       ],
     license='MIT',
+    download_url = 'https://github.com/CaptainMishan/RDRSS/archive/0.2.tar.gz',
     long_description=open('README.md').read(),
 )
